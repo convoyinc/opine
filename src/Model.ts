7c6c8ff0b487717af9d442a29a6b4a5ae2580829
Ian <ian@nevir.net>
2016-02-27 14:54:18 -0800

    Tweak the interface a bit

--- src/Model.ts
+++ src/Model.ts
@@ -1,3 +1,11 @@
+import * as _path from 'path';
+
 export default class Model {
 
+}
+
+export function makeModel(..._pathParts:string[]):typeof Model {
+  return class BuiltModel extends Model {
+
+  };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { default as Model } from './Model';
+export { default as Model, makeModel } from './Model';
